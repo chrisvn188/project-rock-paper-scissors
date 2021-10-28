f806c21809e94e294144e3bbdc99473147718984
ChrisVn188 <chrisbui.dev@gmail.com>
2021-10-28 09:47:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,112 +1,122 @@
+// Author: Chi Anh Bui
+// Created: Oct 28, 2021
+// This program is a game called Rock, Paper and Scissors.
+const NUMBER_OF_SHAPES = 3;
 const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
-const DRAW = "DRAW";
-const COMPUTER_WINS = "COMPUTER WINS";
-const HUMAN_WINS = "HUMAN WINS";
-const CHOICE = [ROCK, PAPER, SCISSORS];
+const DRAW_TEXT = "DRAW";
+const COMPUTER_WINS_TEXT = "COMPUTER WINS";
+const HUMAN_WINS_TEXT = "HUMAN WINS";
+const WRONG_INPUT_TEXT = "WRONG PLAYER INPUT - NOT COUNT";
 
 let humanScore = 0;
 let computerScore = 0;
 
-
-// COMPUTER PLAY AUTOMATICALLY FUNCTION
-let computerPlay = function () {
-    let comChoiceIndex = Math.floor(Math.random() * CHOICE.length);
-    console.log(`Computer choice is ${CHOICE[comChoiceIndex]}`);
-    return CHOICE[comChoiceIndex];
+// A FUNCTION TO GET COMPUTER PLAY AUTOMATICALLY
+let getComputerPlay = function () {
+  let randomNumber = Math.floor(Math.random() * NUMBER_OF_SHAPES);
+  switch (randomNumber) {
+    case 0:
+      console.log(`Computer choice is ${ROCK}`);
+      return ROCK;
+      break;
+    case 1:
+      console.log(`Computer choice is ${PAPER}`);
+      return PAPER;
+      break;
+    case 2:
+      console.log(`Computer choice is ${SCISSORS}`);
+      return SCISSORS;
+      break;
+    default:
+      return "Something Wrong!";
+  }
 };
 
-// PLAYER PLAY FUNCTION
-let playerPlay = function () {
-    let playerChoice = prompt("Choose a choice for the game - ROCK, PAPER or SCISSORS: ").toUpperCase();
-    console.log(`Player choice is ${playerChoice}`)
-    return playerChoice;
-}
+// A FUNCION TO GET PLAYER PLAY
+let getPlayerPlay = function () {
+  let playerChoice = prompt("Enter a choice - rock, paper or scissors: ");
+  if (playerChoice == null || playerChoice == false) {
+    alert(WRONG_INPUT_TEXT);
+  } else if (
+    playerChoice.toUpperCase() !== ROCK &&
+    playerChoice.toUpperCase() !== PAPER &&
+    playerChoice.toUpperCase() !== SCISSORS
+  ) {
+    alert(WRONG_INPUT_TEXT);
+  } else if (
+    playerChoice.toUpperCase() === ROCK ||
+    playerChoice.toUpperCase() === PAPER ||
+    playerChoice.toUpperCase() === SCISSORS
+  ) {
+    console.log(`Player choice is ${playerChoice.toUpperCase()}`);
+    return playerChoice.toUpperCase();
+  }
+};
+// A FUNCTION TO COMPARE RESULTS EACH ROUND OF THE GAME
+let getResults = function (playerChoice, computerChoice) {
+  let playerPlay = playerChoice();
+  let computerPlay = computerChoice();
 
-// COMPARE RESULTS FUNCTION
-let compareResults = function (playerChoice, computerChoice) {
-    let playerPlay = playerChoice();
-    let computerPlay = computerChoice();
+  if (playerPlay === computerPlay) {
+    return DRAW_TEXT;
+  } else if (
+    (playerPlay === ROCK && computerPlay === PAPER) ||
+    (playerPlay === PAPER && computerPlay === SCISSORS) ||
+    (playerPlay === SCISSORS && computerPlay === ROCK)
+  ) {
+    return COMPUTER_WINS_TEXT;
+  } else if (
+    (playerPlay === ROCK && computerPlay === SCISSORS) ||
+    (playerPlay === PAPER && computerPlay === ROCK) ||
+    (playerPlay === SCISSORS && computerPlay === PAPER)
+  ) {
+    return HUMAN_WINS_TEXT;
+  } else return WRONG_INPUT_TEXT;
+};
 
-    if (playerPlay != null && playerPlay != false) {
-        if (playerPlay === ROCK && computerPlay === ROCK) {
-            return DRAW;
-        }
-        else if (playerPlay === ROCK && computerPlay === PAPER) {
-            return COMPUTER_WINS;
-        }
-        else if (playerPlay === ROCK && computerPlay === SCISSORS) {
-            return HUMAN_WINS;
-        }
-        else if (playerPlay === PAPER && computerPlay === PAPER) {
-            return DRAW;
-        }
-        else if (playerPlay === PAPER && computerPlay === ROCK) {
-            return HUMAN_WINS;
-        }
-        else if (playerPlay === PAPER && computerPlay === SCISSORS) {
-            return COMPUTER_WINS;
-        }
-        else if (playerPlay === SCISSORS && computerPlay === SCISSORS) {
-            return DRAW;
-        }
-        else if (playerPlay === SCISSORS && computerPlay === ROCK) {
-            return COMPUTER_WINS;
-        }
-        else if (playerPlay === SCISSORS && computerPlay === PAPER) {
-            return HUMAN_WINS;
-        }
-    }
-}
-
-// CALCULATE SCORES FUNCTION
+// A FUNCTION TO CALCULATE SCORES EACH ROUND
 let calcScore = function (results) {
-    if (results === COMPUTER_WINS) {
-        computerScore++;
-    } else if (results === HUMAN_WINS) {
-        humanScore++;
-    }
-    else{
-        humanScore++;
-        computerScore++;
-    }
+  if (results === WRONG_INPUT_TEXT) {
+    console.log(WRONG_INPUT_TEXT);
+  } else if (results === COMPUTER_WINS_TEXT) {
+    computerScore++;
+  } else if (results === HUMAN_WINS_TEXT) {
+    humanScore++;
+  }
 };
 
-// GAME FUNCTION
-let game = function (compareResults, calcScore) {
-    let result = compareResults(playerPlay, computerPlay);
-    calcScore(result);
-    console.log(`Player's score is ${humanScore} and ${computerScore} for computer.`);
-    console.log(`--------------------- END ---------------------`);
+// A FUNCTION TO SHOW SCORES FOR PLAYER AND COMPUTER
+let game = function (getResults, calcScore) {
+  console.log(`--------------------- START ROUND ---------------------`);
+  let result = getResults(getPlayerPlay, getComputerPlay);
+  calcScore(result);
+  console.log(
+    `Player's score is ${humanScore} and ${computerScore} for computer.`
+  );
+  console.log(`--------------------- END ROUND ---------------------`);
 };
 
-// GET WINNER FUNCTION
-let getWinner = function (humanScore, computerScore) {
-    if (humanScore < computerScore) {
-        console.log(COMPUTER_WINS);
-    }
-    else if (humanScore > computerScore) {
-        console.log(HUMAN_WINS);
-    }
-    else console.log(`Human and computer ${DRAW}`);
-}
+// A FUNCTION TO SHOW THE WINNER
+let showWinner = function (humanScore, computerScore) {
+  console.log(`--------------------- FINAL RESULT -------------------`);
+  if (humanScore < computerScore) {
+    console.log(COMPUTER_WINS_TEXT);
+  } else if (humanScore > computerScore) {
+    console.log(HUMAN_WINS_TEXT);
+  } else console.log(`Human and computer ${DRAW_TEXT}`);
+};
 
-// PLAY GAME FUNCTION
+// A FUNCTION TO PLAY GAME
 let playGame = function () {
-    game(compareResults, calcScore);
-    game(compareResults, calcScore);
-    game(compareResults, calcScore);
-    game(compareResults, calcScore);
-    game(compareResults, calcScore);
-
-    getWinner(humanScore, computerScore);
-}
-
+  game(getResults, calcScore);
+  game(getResults, calcScore);
+  game(getResults, calcScore);
+  game(getResults, calcScore);
+  game(getResults, calcScore);
+  showWinner(humanScore, computerScore);
+};
 
 // CALL THE PLAY GAME FUNCTION
 playGame();
-
-
-
-
